877262a6801bfed1dacf7bd85b5c8d188e624ee4
Chuck Harmston <chuck@chuckharmston.com>
2015-06-02 15:53:04 -0600

    Loosely replace Fluxxor with Flummox.

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,1 +1,11 @@
-module.exports = {};
+// import { Actions } from 'flummox';
+
+// export default class FooActions extends Actions {
+//   async doSomething(path) {
+//   }
+// }
+
+
+module.exports = {
+  // foo: FooActions
+};
--- src/js/stores/index.js
+++ src/js/stores/index.js
@@ -1,1 +1,16 @@
-module.exports = {};
+// import { Store } from 'flummox';
+
+// class FooStore extends Store {
+//   constructor(flux) {
+//     super();
+//   }
+//   doSomething(message) {
+//     this.setState({
+//       messages: message,
+//     });
+//   }
+// }
+
+module.exports = {
+    // foo: FooStore
+};
