fdf422091d90771f990d42731ba44dce0ee549fa
Chuck Harmston <chuck@chuckharmston.com>
2015-06-04 16:34:41 -0600

    Adds basic wizard functionality.

--- src/js/flux.js
+++ src/js/flux.js
@@ -1,9 +1,11 @@
 import {Flummox} from 'flummox';
 
 import LoginActions from './actions/login';
+import WizardActions from './actions/wizard';
 import LoginStore from './stores/login';
 import SiteConfigStore from './stores/siteConfig';
 import UserStore from './stores/user';
+import WizardStore from './stores/wizard';
 
 
 export default class Flux extends Flummox {
@@ -11,9 +13,11 @@ export default class Flux extends Flummox {
     super();
 
     this.createActions('login', LoginActions);
+    this.createActions('wizard', WizardActions);
 
     this.createStore('login', LoginStore, this);
     this.createStore('siteConfig', SiteConfigStore, this);
     this.createStore('user', UserStore, this);
+    this.createStore('wizard', WizardStore, this);
   }
 }
