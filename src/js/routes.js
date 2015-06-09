5b1fc2318cc1bd7715c462b00361158fa2e013ff
Chuck Harmston <chuck@chuckharmston.com>
2015-06-09 13:13:58 -0600

    Adds handler decorator to require login.

--- src/js/routes.js
+++ src/js/routes.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Router from 'react-router';
 
 import App from './components/app';
-import {LoginHandler} from './components/login';
+import {FxaLogin} from './components/login';
+import Login from './components/handlers/login'
+import {loginRequired} from './components/login';
 import SubmissionRedirect from './components/handlers/index';
 import Review from './components/handlers/review';
 import Submission from './components/handlers/submission';
@@ -11,9 +13,10 @@ import Submission from './components/handlers/submission';
 var Route = Router.Route;
 var routes = <Route name="app" path="/" handler={App}>
   <Route handler={SubmissionRedirect}/>
-  <Route name="login" path="/fxa-authorize" handler={LoginHandler}/>
+  <Route name="login" path="/login" handler={Login}/>
+  <Route name="fxaLogin" path="/fxa-authorize" handler={FxaLogin}/>
   <Route name="review" path="/review" handler={Review}/>
-  <Route name="submission" path="/submission" handler={Submission}/>
+  <Route name="submission" path="/submission" handler={loginRequired(Submission)}/>
 </Route>
 
 
