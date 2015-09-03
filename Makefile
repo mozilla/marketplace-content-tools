31fe7064b0c629e0e4091be49c17a91cd89b7160
Kevin Ngo <me@ngokevin.com>
2015-09-03 09:31:06 -0700

    fix up sherlocked

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
-install:
+test:
 	@npm install
 	@npm run build
-
-test: install
 	@npm test
 
-test-sherlocked: install
-	@NODE_ENV=test SUBMISSION_CLIENT_PORT=8000 node_modules/.bin/gulp serve &
+test-sherlocked:
+	@npm install
+	@NODE_ENV=test npm run serve-js &
+	@npm run serve &
 	@sleep 5 && node sherlocked.js
--- sherlocked.js
+++ sherlocked.js
@@ -1,42 +1,48 @@
 // Visual regression test script for Sherlocked.
-var ROOT = 'http://localhost:8680/';
+var ROOT = 'http://localhost:8679/content/';
+
 
 function mobile(client, path) {
-    return client
-        .setViewportSize({width: 320, height: 960})
-        .url(ROOT + (path || ''));
+  return client
+    .setViewportSize({width: 320, height: 960})
+    .url(ROOT + (path || ''));
 }
 
 function desktop(client, path) {
-    return client
-        .setViewportSize({width: 1050, height: 2048})
-        .url(ROOT + (path || ''));
+  return client
+    .setViewportSize({width: 1050, height: 2048})
+    .url(ROOT + (path || ''));
 }
 
 
 require('sherlocked')
 
-.investigate('Submission Tools', function(client) {
-    return desktop(client, 'submission/')
-        .waitForExist('main', 60000);
+.investigate('Firefox OS Add-on Dashboard', function(client) {
+  return desktop(client, 'addon/')
+    .waitForExist('main', 60000);
 })
 
-.investigate('Reviewer Tools', function(client) {
-    return desktop(client, 'review/')
-        .waitForExist('main', 60000);
+.investigate('Firefox OS Add-on Submission Tools', function(client) {
+  return desktop(client, 'addon/submit/')
+    .waitForExist('main', 60000);
+})
+
+.investigate('Firefox OS Add-on Reviewer Tools', function(client) {
+  return desktop(client, 'addon/review/')
+    .waitForExist('main', 60000);
 })
 
 .begin([
-    {
-        browserName: 'firefox',
-        platform: 'OS X 10.9'
-    },
-    {
-        browserName: 'chrome',
-        platform: 'OS X 10.9'
-    },
-    {
-        browserName: 'safari',
-        platform: 'OS X 10.9'
-    }
+  {
+    browserName: 'firefox',
+    platform: 'OS X 10.9'
+  },
+  {
+    browserName: 'chrome',
+    platform: 'OS X 10.9'
+  },
+  {
+    browserName: 'safari',
+    platform: 'OS X 10.9'
+  }
 ]);
