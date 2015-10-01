1dfb2ff060097dd140f64926c2f9c8b70616d73a
Chuck Harmston <chuck@chuckharmston.com>
2015-10-01 16:12:30 -0600

    Copies the footer from Fireplace.

--- src/media/js/site/components/footer.js
+++ src/media/js/site/components/footer.js
@@ -6,10 +6,31 @@ export default class Footer extends React.Component {
     return (
       <footer>
         <p>
-          Except where otherwise noted, content on this site is licensed under
-          the Creative Commons Attribution Share-Alike License v3.0 or any later
-          version.
+          Except where otherwise <a href="http://www.mozilla.org/about/legal.html#site">
+          noted</a>, content on this site is licensed under
+          the <a href="http://creativecommons.org/licenses/by-sa/3.0/">Creative
+          Commons Attribution Share-Alike License v3.0</a> or any
+          later version.
         </p>
+        <nav>
+          <ul>
+            <li>
+              <a href={`${process.env.MKT_ROOT}privacy-policy`}>
+                Privacy Policy
+              </a>
+            </li>
+            <li>
+              <a href={`${process.env.MKT_ROOT}terms-of-use`}>
+                Terms of Use
+              </a>
+            </li>
+            <li>
+              <a href="https://www.mozilla.org/legal/fraud-report/index.html">
+                Report Trademark Abuse
+              </a>
+            </li>
+          </ul>
+        </nav>
       </footer>
     );
   }
