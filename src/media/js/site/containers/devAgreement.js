dc0e3f3c4136103a1a928ec917f09b3eed1135f5
Chuck Harmston <chuck@chuckharmston.com>
2015-10-08 16:46:13 -0600

    Updates developer agreement view to fetch the TOS URL from the server.

--- src/media/js/site/containers/devAgreement.js
+++ src/media/js/site/containers/devAgreement.js
@@ -3,17 +3,50 @@
   its contents.
 */
 import React from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 
+import {getTOS, signTOS} from '../actions/tos';
 import {Page} from '../components/page';
 import TOSIframe from '../components/tos';
 
 
-export default class DeveloperAgreement extends React.Component {
+export class DeveloperAgreement extends React.Component {
+  static propTypes = {
+    getTOS: React.PropTypes.func.isRequired,
+    user: React.PropTypes.object,
+  };
+
+  constructor(props) {
+    super(props);
+    this.props.getTOS();
+  }
+
+  renderTOS() {
+    if (this.props.user.tos.url) {
+      return <TOSIframe url={this.props.user.tos.url}/>;
+    }
+    return (
+      <div className="tos tos--loading">Loading&hellip;</div>
+    );
+  }
+
   render() {
     return (
       <Page title="Marketplace Developer Agreement">
-        <TOSIframe/>
+        {this.renderTOS()}
       </Page>
     );
   }
 };
+
+
+export default connect(
+  state => ({
+    user: state.user,
+  }),
+  dispatch => bindActionCreators({
+    getTOS,
+    signTOS,
+  }, dispatch)
+)(DeveloperAgreement);
