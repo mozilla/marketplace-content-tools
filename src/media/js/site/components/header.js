6bcba9ebf94715b83c9c546f3e36954073fb512d
Kevin Ngo <me@ngokevin.com>
2015-10-14 14:45:52 -0700

    mobilefy nav

--- src/media/js/site/components/header.js
+++ src/media/js/site/components/header.js
@@ -6,34 +6,43 @@ import {LoginButton} from './login';
 
 
 export default class Header extends React.Component {
-  static contextTypes = {
-    router: React.PropTypes.object
+  static propTypes = {
+    authUrl: React.PropTypes.string,
+    email: React.PropTypes.string,
+    loginBeginHandler: React.PropTypes.func.isRequired,
+    loginHandler: React.PropTypes.func.isRequired,
+    logoutHandler: React.PropTypes.func.isRequired,
+    hasSignedTOS: React.PropTypes.bool,
+    isLoggedIn: React.PropTypes.bool.isRequired,
   };
 
   render() {
     const showNav = this.props.isLoggedIn && this.props.hasSignedTOS;
     return (
-      <header className="header">
+      <header className="header" data-header-show-nav={showNav}>
         <h1>
           <a className="header--wordmark" href="/">Firefox Marketplace</a>
           <ReverseLink to="root" className="header--title">
             Content Tools
           </ReverseLink>
         </h1>
-        {showNav && <nav className="header--nav">
-          <ul>
-            <li><ReverseLink to="addon">Firefox OS Add-ons</ReverseLink></li>
-            <li>
-              <ReverseLink to="dev-agreement">Developer Agreement</ReverseLink>
-            </li>
-            <li>
-              <a href="https://developer.mozilla.org/docs/Mozilla/Firefox_OS/Add-ons/Review_critera"
-                 target="_blank">
-                Review Criteria
-              </a>
-            </li>
-          </ul>
-        </nav>}
+        {showNav &&
+          <nav className="header--nav">
+            <ul>
+              <li><ReverseLink to="addon">Firefox OS Add-ons</ReverseLink></li>
+              <li>
+                <ReverseLink to="dev-agreement">Developer Agreement</ReverseLink>
+              </li>
+              <li>
+                <a href="https://developer.mozilla.org/docs/Mozilla/Firefox_OS/Add-ons/Review_critera"
+                   target="_blank">
+                  Review Criteria
+                </a>
+              </li>
+              <HeaderUserNavMobile {...this.props}/>
+            </ul>
+          </nav>
+        }
         <HeaderUserNav {...this.props}/>
       </header>
     );
@@ -44,7 +53,7 @@ export default class Header extends React.Component {
 class HeaderUserNav extends React.Component {
   static propTypes = {
     authUrl: React.PropTypes.string,
-    displayName: React.PropTypes.string,
+    email: React.PropTypes.string,
     loginBeginHandler: React.PropTypes.func.isRequired,
     loginHandler: React.PropTypes.func.isRequired,
     logoutHandler: React.PropTypes.func.isRequired,
@@ -105,5 +114,26 @@ class HeaderUserNav extends React.Component {
 
   render() {
     return this.props.isLoggedIn ? this.renderUserNav() : this.renderAnonNav();
+  }
+}
+
+
+class HeaderUserNavMobile extends HeaderUserNav {
+  renderAnonNav() {
+    return (
+      <noop className="header-user-nav--mobile">
+        <li><LoginButton isSignup={true} {...this.props}/></li>
+        <li><LoginButton {...this.props}/></li>
+      </noop>
+    );
+  }
+
+  renderUserNav() {
+    return (
+      <li className="header-user-nav--mobile"
+          onClick={this.handleLogoutClick.bind(this)}>
+        <a>Logout from {this.props.email}</a>
+      </li>
+    );
   }
 }
