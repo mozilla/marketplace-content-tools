6154e3f0a714b886b9e195c10eb42ab748038765
Kevin Ngo <me@ngokevin.com>
2015-10-20 16:13:18 -0700

    reviewer search

--- src/media/js/addon/containers/review.js
+++ src/media/js/addon/containers/review.js
@@ -4,18 +4,22 @@ import {ReverseLink} from 'react-router-reverse';
 import {bindActionCreators} from 'redux';
 
 import {fetch} from '../actions/review';
+import {reviewerSearch} from '../actions/search';
 import {AddonListingForReview} from '../components/listing';
+import Search from '../components/search';
 import AddonSubnav from '../components/subnav';
 import {addonListSelector} from '../selectors/addon';
-import {Page} from '../../site/components/page';
+import {Page, PageSection} from '../../site/components/page';
 
 
 export class AddonReview extends React.Component {
   static propTypes = {
+    addonSearch: React.PropTypes.object,
     addons: React.PropTypes.array.isRequired,
     fetch: React.PropTypes.func,
     publish: React.PropTypes.func,
     reject: React.PropTypes.func,
+    search: React.PropTypes.func,
     user: React.PropTypes.object,
   };
 
@@ -34,9 +38,17 @@ export class AddonReview extends React.Component {
       <Page className="addon-review"
             subnav={<AddonSubnav user={this.props.user}/>}
             title="Reviewing Firefox OS Add-ons">
-        <AddonListingForReview addons={this.props.addons}
-                               showWaitingTime={true}
-                               linkTo="addon-review-detail"/>
+        <div>
+          <PageSection title="Search">
+            <Search {...this.props.addonSearch} search={this.props.search}/>
+          </PageSection>
+
+          <PageSection title="Review Queue">
+            <AddonListingForReview addons={this.props.addons}
+                                   showWaitingTime={true}
+                                   linkTo="addon-review-detail"/>
+          </PageSection>
+        </div>
       </Page>
     );
   }
@@ -45,9 +57,11 @@ export class AddonReview extends React.Component {
 
 export default connect(
   state => ({
-    addons: addonListSelector(state.addonReview.addons)
+    addons: addonListSelector(state.addonReview.addons),
+    addonSearch: state.addonSearch
   }),
   dispatch => bindActionCreators({
-    fetch
+    fetch,
+    search: reviewerSearch
   }, dispatch)
 )(AddonReview);
