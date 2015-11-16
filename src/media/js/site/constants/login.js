7d14b0e583729a03012e22041194b183ce6b2580
Kevin Ngo <me@ngokevin.com>
2015-11-16 10:49:27 -0800

    fix reviewer perms

--- src/media/js/site/constants/login.js
+++ src/media/js/site/constants/login.js
@@ -1,3 +1,1 @@
-export const LOGIN = 'content_tools_login';
-export const ADDON_REVIEW = [LOGIN, 'content_tools_addon_review'];
-export const CONTENT_TOOLS_LOGIN = [LOGIN];
+export const ADDON_REVIEW = ['content_tools_addon_review'];
